034b1a5e1a2ae50eb99a413cea930a54b35c5d36
alwaugh03 <awaugh@miuandes.cl>
2026-05-25 18:29:46 -0400

    alert cat/ven

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,37 +12,41 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
-  def create
-    @category = Category.new(category_params)
+def create
+  @category = Category.new(category_params)
 
-    if @category.save
-      redirect_to @category, notice: "Category created successfully."
-    else
-      render :new, status: :unprocessable_entity
-    end
+  if @category.save
+    redirect_to @category,
+                notice: "Category created successfully."
+  else
+    flash.now[:alert] = "Please fix the errors below."
+
+    render :new,
+           status: :unprocessable_entity
   end
+end
 
-  def edit
+def update
+  if @category.update(category_params)
+    redirect_to @category,
+                notice: "Category updated successfully."
+  else
+    flash.now[:alert] = "Please fix the errors below."
+
+    render :edit,
+           status: :unprocessable_entity
   end
+end
 
-  def update
-    if @category.update(category_params)
-      redirect_to @category, notice: "Category updated successfully."
-    else
-      render :edit, status: :unprocessable_entity
-    end
+def destroy
+  if @category.destroy
+    redirect_to categories_path,
+                notice: "Category deleted successfully."
+  else
+    redirect_to categories_path,
+                alert: @category.errors.full_messages.to_sentence
   end
-
-  def destroy
-    if @category.destroy
-      redirect_to categories_path,
-                  notice: "Category deleted successfully."
-    else
-      redirect_to categories_path,
-                  alert: @category.errors.full_messages.to_sentence
-    end
-  end
-
+end
   private
 
   def set_category
