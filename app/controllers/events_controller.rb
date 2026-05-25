a067f1d2cf80f70d0988cbd8e070e5244e35eaeb
FranciscoLanglois <flanglois@miuandes.cl>
2026-05-25 19:34:49 -0400

    commit_FL10

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,16 +1,18 @@
 class EventsController < ApplicationController
-before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-def index
-  if params[:category]
-    @events = Event.where(category: params[:category])
-  else
-    @events = Event.all
+  def index
+    if params[:category]
+      @events = Event.where(category: params[:category])
+    else
+      @events = Event.all
+    end
   end
-end
+
   def show
-    @event = Event.find(params[:id])
+    @event = Event.find(params[:id])  
   end
+
   def new
     @event = Event.new
   end
@@ -18,38 +20,33 @@ end
   def create
     @event = Event.new(event_params)
     @event.organizer = User.first
+    
     if @event.save
-        notice: "Event created successfully."
-        redirect_to @event
+      redirect_to @event, notice: "Event created successfully."  
     else
-        alert: @event.errors.full_messages.to_sentence
-        render :new, status: :unprocessable_entity
+      flash.now[:alert] = @event.errors.full_messages.to_sentence  
+      render :new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @event = Event.find(params[:id])
+   
   end
 
   def update
     if @event.update(event_params)
-        notice: "Event updated successfully."
-        redirect_to @event
+      redirect_to @event, notice: "Event updated successfully."  
     else
-        alert: @event.errors.full_messages.to_sentence
-        render :edit, status: :unprocessable_entity
+      flash.now[:alert] = @event.errors.full_messages.to_sentence 
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    
-    @event = Event.find(params[:id])
     if @event.destroy
-      redirect_to events_url,
-      notice: "Event deleted successfully."
+      redirect_to events_url, notice: "Event deleted successfully." 
     else
-      redirect_to events_url,
-      alert: @event.errors.full_messages.to_sentence
+      redirect_to events_url, alert: @event.errors.full_messages.to_sentence 
     end
   end
 
